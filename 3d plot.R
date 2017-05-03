a44b60d33d6c63ead95448683a78416ecd4846c1
kdover <kdover@users.noreply.github.com>
2017-05-03 00:09:44 -0700

    Add files via upload

--- 3d plot.R
+++ 3d plot.R
@@ -1,20 +1,14 @@
+#import libraries
 library(kernlab)
 library(devtools)
-library(plot3D)
-library(ggplot2)
-library(plotly)
 library(scatterplot3d)
-library(spatial)
-library(MASS)
-library(lattice)
-library(rsm)
+library(seewave)
+library(fitdistrplus)
 
-appleData <- read.csv("apple.csv",stringsAsFactors=FALSE)
-msftData <- read.csv("msft.csv",stringsAsFactors=FALSE)
-spData <- read.csv("sp500.csv",stringsAsFactors=FALSE)
-#visaData <- read.csv("visa.csv",stringsAsFactors=FALSE)
+#import data
 testData <- read.csv("WIKI-AAPL.csv",stringsAsFactors=FALSE)
 
+#reorder data
 tempData =testData
 #need to reverse the data
 data = tempData[nrow(tempData),]
@@ -22,63 +16,20 @@ for(i in 2:nrow(tempData)){
 	data = rbind(data,tempData[((nrow(tempData)+1)-i),])
 }
 
-
+#draw out time, price, volume, diff
 time = c(1:nrow(data))
 price = data[,2]
 volume = data[,6]
 diff = ((data[,5] - data[,2])/data[,5])*100
 
-
-
-tempData = cbind(price,time,volume)
-colnames(tempData) = c("x", "y", "z")
-
-pData = cbind(time,price,volume,diff)
-start = 1
-end = length(time)
-
-#surf3D(x = time, y = volume, z = price, main="test")
+#plot the data with respect to time, price, volume
 pdf("3dScatterPlot.pdf")
 myPlot = scatterplot3d(time, price, volume,main = "3D scatterplot",xlab = "time", ylab = "price", zlab = "volume",pch=16,color="steelblue")
 dev.off()
 
-#fit a surface to the data
-tempData.kr <- surf.ls(3,time[start:end],price[start:end],volume[start:end])
-trsurf <- trmat(tempData.kr,20,70,0,1400,1400)
-png(filename="fittedSurface.png")
-persp3D(trsurf$x,trsurf$y,trsurf$z,theta=50,phi=20,axes=TRUE,nticks=5,ticktype="detailed",xlab="Price",ylab="Time",zlab="Volume",main="3D fit") 
-dev.off()
-
-#do the 3d gaussian process
-variance = 0.001
-#do the particle modeling
-pTest <- predict(gausspr(time[start:end],price[start:end],type="regression",var= variance))
-plot(pTest)
-points(time,price)
-
-#vTest <- predict(lm(volume ~ time))
-vTest <- predict(gausspr(time[start:end],volume[start:end],type="regression",var= 4))
-plot(vTest)
-points(time,volume)
-
-
-#dTest <- predict(lm(diff ~ time))
-dTest <- predict(gausspr(time[start:end],diff[start:end],type="regression",var= 4))
-plot(dTest)
-points(time,diff)
-
-
-plotPData = cbind(pTest,vTest,dTest)
-pdf("parametricCurve.pdf")
-scatterplot3d(plotPData,type="l",lwd=3,xlab="Price",ylab = "volume", zlab="difference")
-dev.off()
-
 
 ### do the different distribution calculations
-library(seewave)
-library(fitdistrplus)
 plot(time,price)
-
 #for every 10 days, calc the distribution
 #distribution for the first price/time
 left = price
@@ -108,6 +59,7 @@ index <- c()
 for(i in 1:176){
 	index = c(index, 50*i)
 }
+#plot the means and standard devs for each step
 pdf("meanPlot.pdf")
 plot(index,mean,xlab="index",ylab="mean",main="Mean of the distribution at every 10 points")
 dev.off()
